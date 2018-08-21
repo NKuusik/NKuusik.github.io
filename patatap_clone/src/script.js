c0ef65698cc6c975fcb1cf8914f52e5e87d75c1b
NKuusik <normankuusik@gmail.com>
2018-08-21 15:12:08 +0300

    Add files via upload

---
+++ patatap_clone/src/script.js
@@ -0,0 +1,17 @@
+import './styles.scss';
+import {Howl, Howler} from 'howler';
+import {data} from './keydata.js';
+
+
+
+// let test = new Howl({
+//     src: ['./assets/sounds/bubbles.mp3', './assets/sounds/clay.mp3']
+// });
+
+// $("body").on("keypress", function (e) {
+//     if (e.which === 13) {
+//         test.play();
+//     }
+//   });
+
+
